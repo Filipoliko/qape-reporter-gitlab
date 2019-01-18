25eed3a5eccb915f4f1f2f04e06078ab95a0c709
Satek <filip.satek@firma.seznam.cz>
2019-01-18 16:07:41 +0100

    If QApe already reported an issue with the same error, but the scenario differs. It will add a comment to the existing issue.

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ class GitlabReporter extends EventEmitter {
 		this.on('scenario:end', eventData => this._handleScenarioEnd(eventData));
 	}
 
-	_handleScenarioEnd(eventData) {
+	async _handleScenarioEnd(eventData) {
 		if (eventData.type === 'failing' &&
 			eventData.minified &&
 			!this._isFailureReported(eventData.scenario)
@@ -32,23 +32,61 @@ class GitlabReporter extends EventEmitter {
 
 			this._results.push({ scenario, errors });
 
-			this._postGitlabIssue(scenario, errors);
+			this._getIssueWithSameError(errors)
+				.then(issueId => {
+					this._postGitlabIssue(scenario, errors, issueId);
+				})
 		}
+	}
+
+	_getIssueWithSameError(errors) {
+		return new Promise(resolve => {
+			request({
+				url: `${this._reporterOptions.url}/api/v4/projects/${this._reporterOptions.projectId}/issues?state=opened&labels=QApe`,
+				headers: {
+					'PRIVATE-TOKEN': this._reporterOptions.privateToken
+				}
+			}, (error, response, body) => {
+				if (!body || error || (response && (response.statusCode !== 200))) {
+					console.error('Unable to get issues from gitlab, duplicate issue might be created.')
+					resolve();
+					return;
+				}
+
+				let issues = JSON.parse(body);
+
+				for (let issue of issues) {
+					for (let err of errors) {
+						if (issue.description && issue.description.includes(err.error)) {
+							resolve(issue.iid);
+							return;
+						}
+					}
+				}
+
+				resolve();
+			});
+		});
 	}
 
 	_isFailureReported(scenario) {
 		return !!this._results.find(result => isEqual(result.scenario, scenario));
 	}
 
-	_postGitlabIssue(scenario, errors) {
+	_postGitlabIssue(scenario, errors, issueId) {
+		let url = issueId ?
+			`${this._reporterOptions.url}/api/v4/projects/${this._reporterOptions.projectId}/issues/${issueId}/notes`
+		:
+			`${this._reporterOptions.url}/api/v4/projects/${this._reporterOptions.projectId}/issues`;
+
 		request.post({
 			method: 'POST',
-			url: `${this._reporterOptions.url}/api/v4/projects/${this._reporterOptions.projectId}/issues`,
+			url,
 			headers: {
 				'PRIVATE-TOKEN': this._reporterOptions.privateToken
 			},
 			json: true,
-			body: this._getGitlabRequestBody(scenario, errors)
+			body: this._getGitlabRequestBody(scenario, errors, issueId)
 		}, (error, response) => {
 			if (error) {
 				return console.error(error);
@@ -58,14 +96,31 @@ class GitlabReporter extends EventEmitter {
 				return console.error(`Recieved wrong status code ${response.statusCode}`);
 			}
 
-			console.log('Gitlab Issue successfully created.');
+			if (issueId) {
+				console.log(`Gitlab: Added comment to issue id ${issueId} containing more info on the error.`);
+			} else {
+				console.log('Gitlab: Issue successfully created.');
+			}
 		});
 	}
 
-	_getGitlabRequestBody(scenario, errors) {
+	_getGitlabRequestBody(scenario, errors, issueId) {
+		if (issueId) {
+			return {
+				body: 'QApe found another scenario, which leads to the following error:\n\n' +
+					'```\n' +
+					JSON.stringify(errors, null, '\t') +
+					'\n```' +
+					'\n\nFollowing scenario reproduces the error:\n\n' +
+					this._getScenarioReadable(scenario) +
+					'\n\nJSON:\n\n' +
+					this._getScenarioJSON(scenario, errors)
+				};
+		}
+
 		return {
 			title: `QApe_${new Date().getTime()}`,
-			description: 'QApe found a scenario, which leads to following error:\n\n' +
+			description: 'QApe found a scenario, which leads to the following error:\n\n' +
 				'```\n' +
 				JSON.stringify(errors, null, '\t') +
 				'\n```' +
@@ -74,7 +129,7 @@ class GitlabReporter extends EventEmitter {
 				'\n\nJSON:\n\n' +
 				this._getScenarioJSON(scenario, errors),
 			labels: 'QApe'
-		}
+		};
 	}
 
 	_getScenarioReadable(scenario) {
